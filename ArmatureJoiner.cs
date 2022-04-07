bd5127e8f9b3f11a47feed8eca7068df23040483
Othereum <seokjin.dev@gmail.com>
2022-04-07 23:37:30 +0900

    조인 후 중첩된 트랜스폼을 DynamicBone의 제외 목록에 자동으로 추가

--- ArmatureJoiner.cs
+++ ArmatureJoiner.cs
@@ -16,25 +16,50 @@ namespace OthereumTools
         }
 
 
-        public Transform @base;
-        public Transform additive;
+        public Transform parent;
+        public Transform child;
         public string suffix;
         public bool exactHierarchy;
         public bool keepWorldTransform;
-        Dictionary<string, Transform> bases;
+        bool hasEverSearchedDynamicBones;
+
+        [Space]
+        [Tooltip("Optional: Adds nested bones to the exclusions.")]
+        public List<MonoBehaviour> dynamicBones;
+
+        Dictionary<string, Transform> parentTransforms;
+        Dictionary<Transform, HashSet<SerializedProperty>> exclusionsByDynamicBone;
 
         void OnValidate()
         {
-            isValid = @base != null && additive != null;
+            isValid = parent != null && child != null;
+
+            if (dynamicBones.Any()) {
+                return;
+            }
+
+            if (parent == null) {
+                hasEverSearchedDynamicBones = false;
+            }
+            else if (!hasEverSearchedDynamicBones) {
+                foreach (var behaviour in parent.parent.GetComponentsInChildren<MonoBehaviour>()) {
+                    if (behaviour.GetType().Name == "DynamicBone") {
+                        dynamicBones.Add(behaviour);
+                    }
+                }
+                hasEverSearchedDynamicBones = true;
+            }
         }
 
         void OnWizardCreate()
         {
             if (!exactHierarchy) {
-                bases = new Dictionary<string, Transform>();
-                BuildDictionary(@base);
+                parentTransforms = new Dictionary<string, Transform>();
+                CacheBaseTransforms(parent);
             }
-            foreach (Transform a in additive) {
+            CacheDynamicBones();
+
+            foreach (Transform a in child) {
                 Join(a, "");
             }
         }
@@ -45,9 +70,9 @@ namespace OthereumTools
 
             Transform newbase;
             if (exactHierarchy) {
-                newbase = @base.Find(curpath);
+                newbase = parent.Find(curpath);
             } else {
-                bases.TryGetValue(current.name, out newbase);
+                parentTransforms.TryGetValue(current.name, out newbase);
             }
 
             if (newbase != null) {
@@ -59,6 +84,14 @@ namespace OthereumTools
                     current.localRotation = Quaternion.identity;
                     current.localScale = Vector3.one;
                 }
+                exclusionsByDynamicBone.TryGetValue(newbase, out var properties);
+                if (properties != null) {
+                    foreach (var exclusions in properties) {
+                        var excluded = exclusions.GetArrayElementAtIndex(exclusions.arraySize++);
+                        excluded.objectReferenceValue = current;
+                        exclusions.serializedObject.ApplyModifiedProperties();
+                    }
+                }
             }
 
             foreach (var a in current.Cast<Transform>().ToArray()) {
@@ -66,11 +99,63 @@ namespace OthereumTools
             }
         }
 
-        void BuildDictionary(Transform transform)
+        void CacheBaseTransforms(Transform transform)
         {
-            foreach (Transform child in transform) {
-                bases.Add(child.name, child);
-                BuildDictionary(child);
+            foreach (Transform tr in transform) {
+                try {
+                    parentTransforms.Add(tr.name, tr);
+                } catch (System.ArgumentException) {
+                    Debug.LogWarning("A transform with the same name exists: " + tr);
+                }
+                CacheBaseTransforms(tr);
+            }
+        }
+
+        void CacheDynamicBones()
+        {
+            exclusionsByDynamicBone = new Dictionary<Transform, HashSet<SerializedProperty>>();
+
+            foreach (var db in dynamicBones) {
+                try {
+                    if (db == null) {
+                        continue;
+                    }
+
+                    var so = new SerializedObject(db);
+                    var exclusions = so.FindProperty("m_Exclusions");
+
+                    var root = (Transform)so.FindProperty("m_Root").objectReferenceValue;
+                    AddExclusionsProperty(root, exclusions);
+
+                    var roots = so.FindProperty("m_Roots");
+                    for (int i = 0; i < roots.arraySize; i++) {
+                        var r = (Transform)roots.GetArrayElementAtIndex(i).objectReferenceValue;
+                        AddExclusionsProperty(r, exclusions);
+                    }
+
+                    Undo.RecordObject(db, Title);
+                }
+                catch {
+                    Debug.LogWarning("It's not a dynamic bone: " + db);
+                }
+            }
+        }
+
+        void AddExclusionsProperty(Transform root, SerializedProperty exclusions)
+        {
+            if (root == null) {
+                return;
+            }
+
+            exclusionsByDynamicBone.TryGetValue(root, out var properties);
+            if (properties == null) {
+                properties = new HashSet<SerializedProperty>();
+                exclusionsByDynamicBone[root] = properties;
+            }
+            properties.Add(exclusions);
+
+            foreach (Transform tr in root) {
+                AddExclusionsProperty(tr, exclusions);
             }
         }
     }
